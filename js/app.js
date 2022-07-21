f3219452a82e83e93adb18e090a76ab461d1c861
Bilobra <bra.carrozzo@gmail.com>
2022-07-21 17:37:58 +0200

    add inner scount + inner vagon

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ console.log(buttonElement)
 // elementi ticket
 let nameSurname = document.getElementById('name_surname')
 let ticketPrice = document.getElementById('ticket_price')
+let scountTicket = document.getElementById('sconto')
+let numberVagone = document.getElementById('carrozza')
 
 // event + click
 buttonElement.addEventListener('click',
@@ -26,10 +28,12 @@ buttonElement.addEventListener('click',
     let prezzo = formKm.value * PREZZO_AL_CHILOMETRO
 
     if(formAge.value == 'Minorenne'){
-        prezzo = prezzo - ( prezzo * 0.2 ) 
-        console.log(prezzo)
+        prezzo = prezzo - ( prezzo * 0.2 );
+        scountTicket.innerHTML= 'sconto  20%';
+       
     } else if (formAge.value == 'Over65'){
-        prezzo = prezzo - ( prezzo * 0.4 )
+        prezzo = prezzo - ( prezzo * 0.4 );
+        scountTicket.innerHTML= 'sconto  40%';
     }
     else{
         prezzo = prezzo
@@ -37,6 +41,8 @@ buttonElement.addEventListener('click',
     
     nameSurname.innerHTML = formName.value
     ticketPrice.innerHTML = prezzo.toFixed(2) + '\u20AC'
+    numberVagone.innerHTML = Math.floor(Math.random() * 50)
+    
 })
 
 
